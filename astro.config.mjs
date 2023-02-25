10cf6bde30547733c640cb31fe545f7e7bc32e2c
jonDatz <notjondatz@gmail.com>
2023-02-24 22:16:21 -0500

    First Commit
    First commit for blog. Astro blog with react and tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), tailwind()]
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Blog by jonDatz";
+export const SITE_DESCRIPTION =
+  "New to webdev, jonDatz's blog for all things code and D&D";
